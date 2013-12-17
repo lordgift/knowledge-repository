01d2a3651f81dbdf3f9dbe9f0ac9bd23b547ef77
Gift <Gift@10.59.202.67>
2013-12-17 14:00:49 +0700

    parse success

--- XMLToSQLite/src/app/main/XMLToSQLite.java
+++ XMLToSQLite/src/app/main/XMLToSQLite.java
@@ -12,15 +12,15 @@ public class XMLToSQLite {
 	
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
 		
-		XMLParser xmlET = XMLParser.getInstance(Eng2Thai.class);
-		List<Object> eng2ThaiList = xmlET.parseDocument("lexitron-data/etlex_fixed");
-				
-//		XMLParser xmlTE = XMLParser.getInstance(Thai2Eng.class);
-//		List<Object> thai2EngList = xmlTE.parseDocument("lexitron-data/telex_fixed");
+//		XMLParser xmlET = XMLParser.getInstance(Eng2Thai.class);
+//		List<Object> eng2ThaiList = xmlET.parseDocument("lexitron-data/etlex_fixed");
+//				
+		XMLParser xmlTE = XMLParser.getInstance(Thai2Eng.class);
+		List<Object> thai2EngList = xmlTE.parseDocument("lexitron-data/telex_fixed");
 		
 //		SQLiteManager.createTable();		
-		SQLiteManager.insertValueByList(eng2ThaiList);
-//		SQLiteManager.insertValueByList(thai2EngList);
+//		SQLiteManager.insertValueByList(eng2ThaiList);
+		SQLiteManager.insertValueByList(thai2EngList);
 		
 	}
 
